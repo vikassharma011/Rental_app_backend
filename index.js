827f5192125bc6611bbdee646814b000be32025b
vikassharma011 <vikassharma9669@gmail.com>
2025-07-17 16:47:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // src/index.ts
 import express from "express";
 import cors from "cors" ; 
-// import passport from "passport";
+import * as passport from "passport";
 import { googleAuthRoutes } from "./Routes/auth/google.js";
 import {auth} from "./Routes/auth/Auth.js";
 import  "./Routes/auth/passport.js"; // ✅ Passport import karo
