93a05329b1984deb6a5afa212a92fe848497db89
vikassharma011 <vikassharma9669@gmail.com>
2025-07-18 18:39:42 +0530

    Update Property.js

--- Routes/Investor/Property.js
+++ Routes/Investor/Property.js
@@ -1,5 +1,5 @@
 import express from "express" ; 
-import db from "../../db.js";
+import { db } from "../../db.js";
 import { authenticateInvestor } from "../../middlewares/authenticateInvestor.js";
 
 const router = express.Router();
