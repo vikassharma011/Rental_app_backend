29c3c31b8038cdd57a984ea2715f57b1e0151ed2
vikassharma011 <vikassharma9669@gmail.com>
2025-07-18 18:48:30 +0530

    Update Property.js

--- Routes/Investor/Property.js
+++ Routes/Investor/Property.js
@@ -24,7 +24,7 @@ router.post("/add/property", async (req, res) => {
     }
 
     const [result] = await db.execute(
-      `INSERT INTO properties (
+      `INSERT INTO property (
         title, image, address, city, state, zip_code,
         investor_id, description
       ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
