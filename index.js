2cd5b65ce35a4bce50d08ff7e7f0a1e9fd18110d
vikassharma011 <vikassharma9669@gmail.com>
2025-08-18 22:58:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketIOServer(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true
   }
 });
